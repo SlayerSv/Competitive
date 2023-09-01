be04ec6455023315f201507193f886462423ce7a
SlayerSv <slayer-sv@mail.ru>
2023-09-01 16:09:40 +0700

    renewed template

--- Interview-test-problems/template.js
+++ Interview-test-problems/template.js
@@ -1,10 +1,24 @@
+function solve(input) {
+  let answer;
+  
+  return answer;
+}
+  
 const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin
+  input: process.stdin
 });
-
+  
+let lineNumber = -1;
+  
 rl.on("line", line => {
+  if (lineNumber === -1) {
+    // skip
+  } else if (lineNumber % 2 === 0){
     
-}).on("close", () => {
-    process.stdout.write();
+  } else {
+    const answer = solve(line);
+    console.log(answer.toString());
+  }
+  lineNumber++;
 });
