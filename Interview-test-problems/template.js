a3cabc8a54d205ddfc17de3a4fbd6c190395db40
SlayerSv <slayer-sv@mail.ru>
2023-09-03 03:49:35 +0700

    updated template

--- Interview-test-problems/template.js
+++ Interview-test-problems/template.js
@@ -9,16 +9,26 @@ const rl = readline.createInterface({
   input: process.stdin
 });
   
-let lineNumber = -1;
+let lineNumber = 0;
+// let meta = 0;
+// let data = [];
+// let lastData = 1;
   
 rl.on("line", line => {
   if (lineNumber === -1) {
-    // skip
   } else if (lineNumber % 2 === 0){
-    
+
+    // lastData = +line;
+    // meta = lastData + 1;
   } else {
     const answer = solve(line);
     console.log(answer.toString());
+    // data.push(line);
+    // if (lineNumber === lastData) {
+    //   const answer = solve(data);
+    //   console.log(answer.toString());
+    //   data = [];
+    // }
   }
   lineNumber++;
 });
