d681dd4ecb36e07efe71de3f69e9815362d15d5a
SlayerSv <slayer-sv@mail.ru>
2023-09-10 05:06:38 +0700

    updated template

--- template.js
+++ template.js
@@ -1,20 +1,31 @@
-function solve(input) {
+function solve() {
   let ans;
   
+
+
+
+
   return ans;
 }
-  
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin
-});
-  
+
+function prepareData(data) {
+  data[0] = data[0].split(" ").map(Number);
+
+
+
+  return data;
+}
+
 let lineNumber = -1;
 let meta = 0;
 let lastData = -2;
 let data = [];
 
-  
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
 rl.on("line", line => {
   if (lineNumber === -1) {
 
@@ -22,23 +33,27 @@ rl.on("line", line => {
       if (lastData < 0) {
         meta = lineNumber + 2;
       } else {
-        lastData = lineNumber + line;
+        lastData = lineNumber + Number(line);
         meta = lastData + 1;
       }
       
   } else {
-    if (lastData < 0) {
-      const [a, b, c] = line.split(" ").map(Number);
-      const answer = solve(a, b, c);
-      console.log(answer);
-    } else {
-      data.push(line);
-      if (lineNumber === lastData) {
-        const answer = solve(data);
-        console.log(answer.toString());
-        data = [];
-      }
+    data.push(line);
+    if (lastData < 0 || lineNumber === lastData) {
+      const preparedData = prepareData(data);
+      const answer = solve(...preparedData);
+      printAnswer(answer);
+      data = [];
     }
   }
   lineNumber++;
 });
+
+function printAnswer(answer) {
+  if (Array.isArray(answer)) {
+    console.log(answer.join(" "));
+  }
+  else {
+    console.log(answer.toString());
+  }
+}
