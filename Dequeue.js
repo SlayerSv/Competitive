db9ee9f9e6986929da0e11cfc36ee210194bd106
SlayerSv <slayer-sv@mail.ru>
2023-09-18 09:57:36 +0700

    Added Dequeue and Heap classes

---
+++ Dequeue.js
@@ -0,0 +1,129 @@
+class Dequeue {
+
+  constructor(capacity = 20) {
+    this.dequeue = new Array(capacity);
+    this.head = capacity - 1;
+    this.tail = 0;
+    this.size = 0;
+  }
+
+  get length() {
+    return this.size;
+  }
+
+  pushBack(value) {
+    if (this.size === this.dequeue.length) {this.expand();}
+    this.dequeue[this.tail] = value;
+    this.tail = (this.tail + 1) % this.dequeue.length;
+    this.size++;
+  }
+
+  pushFront(value) {
+    if (this.size === this.dequeue.length) {this.expand();}
+    this.dequeue[this.head] = value;
+    this.head = (this.head + this.dequeue.length - 1) % this.dequeue.length;
+    this.size++;
+  }
+
+  popBack() {
+    if (this.size === 0) {return undefined;}
+    this.tail = (this.tail + this.dequeue.length - 1) % this.dequeue.length;
+    this.size--;
+    return this.dequeue[this.tail];
+  }
+
+  popFront() {
+    if (this.size === 0) {return undefined;}
+    this.head = (this.head + 1) % this.dequeue.length;
+    this.size--;
+    return this.dequeue[this.head];
+  }
+
+  peekFront() {
+    if (this.size === 0) {
+      return null;
+    }
+    return this.dequeue[(this.head + 1) % this.dequeue.length]
+  }
+
+  peekBack() {
+    if (this.size === 0) {
+      return null;
+    }
+    return this.dequeue[(this.tail + this.dequeue.length - 1) % this.dequeue.length]
+  }
+
+  expand() {
+    const newDequeue = new Array(this.dequeue.length * 2);
+    for (let i = 0; i < this.dequeue.length; i++) {
+      this.head = (this.head + 1) % this.dequeue.length;
+      newDequeue[i] = this.dequeue[this.head];
+    }
+    this.dequeue = newDequeue;
+    this.head = this.dequeue.length - 1;
+    this.tail = this.size;
+  }
+
+  getArray() {
+    const normalizedArr = [];
+    let tmp = this.head;
+    for (let i = 0; i < this.size; i++) {
+      normalizedArr.push(this.dequeue[++tmp % this.dequeue.length])
+    }
+    return normalizedArr;
+  }
+}
+
+
+// TESTING
+
+
+let dequeue = new Dequeue(5);
+console.log("\nTESTING BACK\n");
+for (let i = 0; i < 10; i++) {
+  dequeue.pushBack(i);
+}
+console.log(dequeue.dequeue, dequeue.peekBack(), dequeue.peekFront());
+
+dequeue = new Dequeue(5);
+console.log("\nTESTING FRONT\n");
+for (let i = 0; i < 10; i++) {
+  dequeue.pushFront(9 - i);
+}
+console.log(dequeue.dequeue, dequeue.peekBack(), dequeue.peekFront());
+
+dequeue = new Dequeue(5);
+console.log("\nTESTING FRONT AND BACK\n");
+for (let i = 0; i < 5; i++) {
+  dequeue.pushFront(4 - i);
+  dequeue.pushBack(i + 5);
+
+}
+console.log(dequeue.dequeue, dequeue.peekBack(), dequeue.peekFront());
+
+console.log("\nPOPING FRONT\n");
+for (let i = 0; i < 10; i++) {
+  console.log(dequeue.popFront());
+}
+
+console.log("\nREFILLING\n");
+for (let i = 0; i < 5; i++) {
+  dequeue.pushFront(4 - i);
+  dequeue.pushBack(i + 5);
+}
+console.log(dequeue.dequeue);
+
+console.log("\nPOPING BACK\n");
+
+for (let i = 0; i < 10; i++) {
+  console.log(dequeue.popBack());
+}
+
+console.log(dequeue.peekBack(), dequeue.peekFront());
+
+console.log("\nREFILLING\n");
+for (let i = 0; i < 5; i++) {
+  dequeue.pushFront(4 - i);
+  dequeue.pushBack(i + 5);
+}
+console.log(dequeue.dequeue, dequeue.peekBack(), dequeue.peekFront());
---
+++ Heap.js
@@ -0,0 +1,84 @@
+class Heap {
+
+  constructor(array = [], compareFunc = (a, b) => a < b) {
+    this.heap = array;
+    this.needSwap = compareFunc;
+    this.heapify();
+  }
+
+  down(i) {
+    let childIndex = i * 2 + 1;
+    while (childIndex < this.heap.length) {
+      if (childIndex + 1 < this.heap.length
+          && this.needSwap(this.heap[childIndex + 1], this.heap[childIndex])) {
+        childIndex++;
+      }
+      if (this.needSwap(this.heap[childIndex], this.heap[i])) {
+        const tmp = this.heap[i]
+        this.heap[i] = this.heap[childIndex];
+        this.heap[childIndex] = tmp;
+      } else {
+        break;
+      }
+      i = childIndex;
+      childIndex = i * 2 + 1;
+    }
+  }
+
+  up(i) {
+    let parentIndex = (i - 1) >> 1;
+    while (i !== 0) {
+      if (this.needSwap(this.heap[i], this.heap[parentIndex])) {
+        const tmp = this.heap[parentIndex]
+        this.heap[parentIndex] = this.heap[i];
+        this.heap[i] = tmp;
+      } else {
+        break;
+      }
+      i = parentIndex;
+      parentIndex = (i - 1) >> 1;
+    }
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.up(this.heap.length - 1);
+  }
+
+  pop() {
+    const value = this.heap[0];
+    this.heap[0] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.down(0);
+    return value;
+  }
+  
+  heapify() {
+    if (this.heap.length < 2) {
+      return;
+    }
+    for (let i = this.heap.length >> 1; i >= 0; i--) {
+      this.down(i);
+    }
+  }
+}
+
+// TESTING
+
+const heap = new Heap();
+
+for (let i = 0; i < 10; i++) {
+  heap.push(10 - i);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(heap.heap);
+  console.log(heap.pop());
+}
+
+const heap1 = new Heap([5, 3, 27, 99, 45, 1, 0, 20])
+
+for (let i = 0; i < 10; i++) {
+  console.log(heap1.heap);
+  console.log(heap1.pop());
+}
