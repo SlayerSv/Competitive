592cd3d0c08d3011319d20c611a445c20ace78be
SlayerSv <slayer-sv@mail.ru>
2024-06-28 10:12:13 +0700

    added binary search functions

---
+++ go/BinarySearch/BinarySearch.go
@@ -0,0 +1,29 @@
+package main
+
+// returns index of the first element that is not less than x
+func leftBinSearch(stops []int, x int) int {
+	l, r := -1, len(stops)
+	for r-l > 1 {
+		mid := (r + l) / 2
+		if x <= stops[mid] {
+			r = mid
+		} else {
+			l = mid
+		}
+	}
+	return r
+}
+
+// returns index of the first element that is greater than x
+func rightBinSearch(stops []int, x int) int {
+	l, r := -1, len(stops)
+	for r-l > 1 {
+		mid := (r + l) / 2
+		if x < stops[mid] {
+			r = mid
+		} else {
+			l = mid
+		}
+	}
+	return r
+}
