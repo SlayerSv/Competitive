8d3bd28716f693334d73e7926c0e3584f869fc91
SlayerSv <slayer-sv@mail.ru>
2024-07-05 16:35:34 +0700

    changed underlying type to int64 from struct

--- go/dataStructures/bitMask/bitMask.go
+++ go/dataStructures/bitMask/bitMask.go
@@ -1,27 +1,25 @@
 package bitmask
 
-type bitmask struct {
-	mask int64
-}
+type bitmask int64
 
 func (bm bitmask) Set(i, val int) {
 	if val == 1 {
-		bm.mask |= 1 << i
+		bm |= 1 << i
 	} else if val == 0 {
-		bm.mask &= ^(1 << i)
+		bm &= ^(1 << i)
 	}
 }
 
 func (bm bitmask) IsBit(i int) bool {
-	return bm.mask&(1<<i) != 0
+	return bm&(1<<i) != 0
 }
 
 func (bm bitmask) Invert(i int) {
-	bm.mask ^= 1 << i
+	bm ^= 1 << i
 }
 
 func (bm bitmask) Count() int {
-	c := bm.mask
+	c := bm
 	count := 0
 	for c != 0 {
 		c &= c - 1
