c9eb7fa0aa4611526872c7d9f7f5e92b0f25c1b7
SlayerSv <slayer-sv@mail.ru>
2024-07-28 09:51:16 +0700

    feat: add nextString, nextLIne, next2Int functions,

--- go/template/template.go
+++ go/template/template.go
@@ -2,54 +2,92 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func solve() {
+	ans := 0
 
+	fmt.Fprintln(w, ans)
 }
 
 func main() {
+	defer w.Flush()
 	t := 1
-	//t = next()
+	//t = nextInt()
 	for t > 0 {
 		solve()
 		t--
 	}
-	w.Flush()
+
 }
+
+var r *bufio.Reader
+var w *bufio.Writer
 
 func init() {
 	in, err := os.Open("input.txt")
-	if err == nil {
-		r = bufio.NewReader(in)
+	if err != nil {
+		in = os.Stdin
 	}
+	r = bufio.NewReader(in)
+	out, err := os.OpenFile("output.txt", os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		out = os.Stdout
+	}
+	w = bufio.NewWriter(out)
 }
 
-var r = bufio.NewReader(os.Stdin)
-var w = bufio.NewWriter(os.Stdout)
-
-func next() int {
-	var n int
-	fmt.Fscan(r, &n)
-	return n
+func nextIntSlice() []int {
+	line := nextLineSlice()
+	subs := bytes.Fields(line)
+	s := make([]int, 0, len(subs))
+	for i := range subs {
+		val, _ := strconv.Atoi(string(subs[i]))
+		s = append(s, val)
+	}
+	return s
 }
 
-func nexts() []byte {
-	var n string
-	fmt.Fscan(r, &n)
-	return []byte(n)
+func nextLineSlice() []byte {
+	line, hasMore, _ := r.ReadLine()
+	if hasMore {
+		tmp := slices.Clone(line)
+		for hasMore {
+			line, hasMore, _ = r.ReadLine()
+			tmp = append(tmp, line...)
+		}
+		line = tmp
+	}
+	return line
 }
 
-func nextf() float64 {
-	var n float64
-	fmt.Fscan(r, &n)
-	return n
+func nextLine() []byte {
+	return bytes.Clone(nextLineSlice())
 }
 
-func print(n ...interface{}) {
-	fmt.Fprintln(w, n...)
+func nextString() string {
+	s := nextLineSlice()
+	return strings.TrimSpace(string(s))
+}
+
+func nextInt() int {
+	line := nextLineSlice()
+	val, _ := strconv.Atoi(string(line))
+	return val
+}
+
+func next2Int() (int, int) {
+	line := nextLineSlice()
+	nums := bytes.Fields(line)
+	v1, _ := strconv.Atoi(string(nums[0]))
+	v2, _ := strconv.Atoi(string(nums[1]))
+	return v1, v2
 }
 
 func Min[T int | float64](b ...T) (min T) {
