834ab9a867059878bd0d1b610709fb3312a41d3e
SlayerSv <slayer-sv@mail.ru>
2024-08-08 13:46:28 +0700

    feat: add mst func

---
+++ go/algs/graphs/mst.go
@@ -0,0 +1,24 @@
+package graphs
+
+func mst(graph [][][2]int) {
+	n := len(graph)
+	connected := make([]bool, n)
+	q := heap{}
+	q.Push(makeNode(0, 0))
+	var node Node
+	for n > 0 {
+		node = q.Pop()
+		if connected[node.v] {
+			continue
+		}
+		visited[node.v] = true
+		n--
+		for _, neigh := range graph[node.v] {
+			to, dist := neigh[0], neigh[1]
+			if visited[to] {
+				continue
+			}
+			q.Push(makeNode(to, dist))
+		}
+	}
+}
