061ad1a1e2c124cdf0514fbcf38f400fc9bda172
SlayerSv <slayer-sv@mail.ru>
2024-08-08 23:18:53 +0700

    refactor: move dependencies to func

--- go/algs/graphs/bridges.go
+++ go/algs/graphs/bridges.go
@@ -1,30 +1,37 @@
 package graphs
 
-var n int
-var graph = make([][]int, n)
-var visited = make([]bool, n)
-var tin = make([]int, n)
-var lowest = make([]int, n)
-var bridges = [][2]int{}
-var time = 0
-
-func findBridges(node, parent int) {
-	visited[node] = true
-	tin[node] = time
-	lowest[node] = time
-	time++
-	for _, neigh := range graph[node] {
-		if parent == neigh {
-			continue
-		}
-		if visited[neigh] {
-			lowest[node] = min(lowest[node], tin[neigh])
-		} else {
-			findBridges(neigh, node)
-			lowest[node] = min(lowest[node], lowest[neigh])
-			if lowest[neigh] > tin[node] {
-				bridges = append(bridges, [2]int{node, neigh})
+func findBridges(graph [][]int) [][2]int {
+	n := len(graph)
+	visited := make([]bool, n)
+	lowest := make([]int, n)
+	bridges := [][2]int{}
+	tin := make([]int, n)
+	time := 0
+	var dfs func(int, int)
+	dfs = func(node, parent int) {
+		visited[node] = true
+		tin[node] = time
+		lowest[node] = time
+		time++
+		for _, neigh := range graph[node] {
+			if parent == neigh {
+				continue
+			}
+			if visited[neigh] {
+				lowest[node] = min(lowest[node], tin[neigh])
+			} else {
+				dfs(neigh, node)
+				lowest[node] = min(lowest[node], lowest[neigh])
+				if lowest[neigh] > tin[node] {
+					bridges = append(bridges, [2]int{node, neigh})
+				}
 			}
 		}
 	}
+	for i := 0; i < n; i++ {
+		if !visited[i] {
+			dfs(i, i)
+		}
+	}
+	return bridges
 }
