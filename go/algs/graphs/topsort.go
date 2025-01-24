543a080185cb47bfe9ce10bd97185e890fc4e702
SlayerSv <slayer-sv@mail.ru>
2025-01-25 05:39:51 +0700

    [go]: [topsort]: fix: change naming, add capacity for slices to reduce reallocation and copying

--- go/algs/graphs/topsort.go
+++ go/algs/graphs/topsort.go
@@ -1,28 +1,28 @@
 package graphs
 
-func topsort(graph [][]int) ([]int, bool) {
-	n := len(graph) + 1
+func kahn(graph [][]int) ([]int, bool) {
+	n := len(graph) + 1 // if vertices start with 0 then n := len(graph)
 	indegrees := make([]int, n)
-	for _, vertices := range graph {
-		for _, v := range vertices {
-			indegrees[v]++
+	for _, from := range graph {
+		for _, to := range from {
+			indegrees[to]++
 		}
 	}
-	q := []int{}
+	q := make([]int, 0, n)
 	for i := 1; i < n; i++ {
 		if indegrees[i] == 0 {
 			q = append(q, i)
 		}
 	}
-	path := []int{}
+	path := make([]int, 0, n)
 	for len(q) > 0 {
 		node := q[0]
 		q = q[1:]
 		path = append(path, node)
-		for _, v := range graph[node] {
-			indegrees[v]--
-			if indegrees[v] == 0 {
-				q = append(q, v)
+		for _, to := range graph[node] {
+			indegrees[to]--
+			if indegrees[to] == 0 {
+				q = append(q, to)
 			}
 		}
 	}
