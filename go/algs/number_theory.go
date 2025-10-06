be92ed833cb200b1d8fac100f57f492d088ad531
SlayerSv <slayer-sv@mail.ru>
2025-10-06 10:52:13 +0700

    [go]: feat: add phi func

--- go/algs/number_theory.go
+++ go/algs/number_theory.go
@@ -18,3 +18,26 @@ func gcd(a, b int) int {
 		a, b = b, a
 	}
 }
+
+func phi(n int) int {
+	ans := n
+	for i := 2; i*i <= n; i++ {
+		for n%i == 0 {
+			n /= i
+		}
+		ans -= ans / i
+	}
+	if n > 1 {
+		ans -= ans / n
+	}
+	return ans
+}
+
+func digitSum(n int) int {
+	ans := 0
+	for n > 0 {
+		ans += n % 10
+		n /= 10
+	}
+	return ans
+}
